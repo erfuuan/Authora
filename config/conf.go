9cbb0916243e477c0be147676a485c5a0389299a
erfuuan <erfuuan@gmail.com>
2025-03-23 15:10:00 +0330

    add config

---
+++ config/conf.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ServerPort string
+	BotToken   string
+	DBPort     string
+	DBHost     string
+	DBPassword string
+	DBName     string
+	DBUser     string
+	SslMode    string
+}
+
+func loadConf() *Config {
+
+	if err := godotenv.Load(".env"); err != nil {
+		log.Println("Warning: No .env file found, using system environment variables.")
+	}
+
+	return &Config{
+		ServerPort: os.Getenv("PORT"),
+		BotToken:   os.Getenv("BOT_TOKEN"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBUser:     os.Getenv("DB_USER"),
+		SslMode:    os.Getenv("SSL_MODE"),
+	}
+
+}
